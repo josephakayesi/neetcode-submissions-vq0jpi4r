8c11cfdbe9b35bc29a91a2e07863018d9353666d
Joseph Akayesi <josephakayesi@gmail.com>
2026-06-29 20:20:28 +0000

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        """
+        - Keep two pointers moving from opposite directions towards the center.
+        - left and right pointer.
+        - keep moving the pointers to the center whilst the character is a valid alphabet and equals each other.
+        - If equal move both; if not equal return false.
+        - If either left or right encounters invalid character move the pointers one step.
+        """
+
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            if s[l].isalnum() and s[r].isalnum() and s[l].lower() != s[r].lower():
+                return False
+
+            if not s[l].isalnum():
+                l += 1
+                continue
+
+            if not s[r].isalnum():
+                r -= 1
+                continue
+
+            if s[l].lower() == s[r].lower():
+                l += 1
+                r -= 1
+
+        return True
